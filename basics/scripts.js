825a6c33855db02c0269013b876cf2a7d657c423
Lavro Sandor <lavro.sandor@BOLYAI.LOCAL>
2024-11-29 15:31:28 +0100

    Hay un gato en mis pantalones

--- basics/scripts.js
+++ basics/scripts.js
@@ -1,27 +1,53 @@
-function Player(nickname){
-    this.nickname = nickname;
-    this.playedmatch = 0;
+// function Player(nickname){
+//     this.nickname = nickname;
+//     this.playedmatch = 0;
 
-}
-Player.prototype.play = function(){
-   this.playedmatch++;
-   console.log(this.nickname, this.playedmatch) 
-}
+// }
+// Player.prototype.play = function(){
+//    this.playedmatch++;
+//    console.log(this.nickname, this.playedmatch) 
+// }
 
 
 
-Player.prototype.getTierLevel = function(){
-    if(this.playedmatch < 4){
-        return "A";
-    }
-    else if(this.playedmatch > 4 && this.playedmatch<7){
-        return "B"
-    }
-    else{
-        return "C"
-    }
+// Player.prototype.getTierLevel = function(){
+//     if(this.playedmatch < 4){
+//         return "A";
+//     }
+//     else if(this.playedmatch > 4 && this.playedmatch<7){
+//         return "B"
+//     }
+//     else{
+//         return "C"
+//     }
  
+// }
+
+
+class Player{
+    constructor(nickname){
+        this.nickname = nickname;
+        this.playedmatch = 0
+        
+    }
+    play(){
+        this.playedmatch++;
+        console.log(this.nickname, this.playedmatch) 
+    }
+    getTierLevel(){
+        if(this.playedmatch < 4){
+                    return "A";
+                }
+                else if(this.playedmatch > 4 && this.playedmatch<7){
+                    return "B"
+                }
+                else{
+                    return "C"
+                }
+    }
 }
+
+
 const gomszab = new Player("gomszab")
 
 gomszab.play();gomszab.play();gomszab.play();gomszab.play();gomszab.play();gomszab.play();gomszab.play();
