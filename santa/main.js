7bdb76cbdce93ad15ca00d496c028ac55ab8eb3f
Lavro Sandor <lavro.sandor@BOLYAI.LOCAL>
2024-12-06 15:35:30 +0100

    Orai 6osig kesz

--- santa/main.js
+++ santa/main.js
@@ -45,14 +45,27 @@ document.getElementById('product').addEventListener('submit',function(e){
 */
 // TODO 6
 function initTable(){
-     for(let c=0; c<companionList; c++){
+     for(let c=0; c<companionList.length; c++){
 
       const currentElement = companionList[c]//Indexelés, Változóba teszi a lista aktuális elemét
 
-      const companion = new Companion(c, currentElement.firstName, currentElement.lastName, currentElement.area)//
+      const companion = new Companion(c, currentElement.firstName, 
+         currentElement.lastName, 
+         currentElement.area)//Példányosítjuk a Companiont. Ezekkel a currentElement adott paramétereivel meghívjuk a megadott paramétereket csak az itt megadott változónevekkel és abban a sorrendben ahogy megadtuk a classesba
+
+
+         for(const pr of currentElement.products)//Azért kell currentElement mert csak egyesével adjuk a companionhoz a productokat 
+         {
+            companion.addProduct(pr)//A companion pédánynak az addProductját meghívjuk és a for ciklusban megnevezett produktumot
+
+
+      
+         }
+         factory.addMano(companion)
+         
 
      }  
-        
+        console.log(factory)
 }
 
 
