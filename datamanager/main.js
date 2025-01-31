4a442faf85e1b07cb4aba1e594b860b5ccdcbede
Ronin021 <lavro.sandor@bolyai.local>
2025-01-31 08:13:52 +0100

    datamanager első óra full

---
+++ datamanager/main.js
@@ -0,0 +1,22 @@
+
+/**
+ * @typedef{nev: String, eletkor: Number} Person
+ * @callback UpdateCallback
+ * @param {Person[]} persons
+ * @returns {void}
+ */
+class Datamanager{
+    /**
+     * @type {Person[]}
+     */
+    #array
+/**
+ * @type {UpdateCallback}
+ */
+    #privateproperty
+
+}
+
+class Datatable{
+
+}
