7ff748f30a5533c4208dd3368dcf913fe2ee902a
Ronin021 <lavro.sandor@BOLYAI.LOCAL>
2025-01-31 09:22:32 +0100

    datamanager második óra full

--- datamanager/main.js
+++ datamanager/main.js
@@ -13,9 +13,64 @@ class Datamanager{
 /**
  * @type {UpdateCallback}
  */
-    #privateproperty
+    #Updatecallback
+
+/**
+ * 
+ * @param {Person[]} array 
+ */
+    constructor(array = []) {
+        this.#array = [];
+     this.UpdateCallback = () =>{}   
+    }
+
+/**
+ * 
+ * @param {Callback} Callback 
+ */
+ setUpdateCallback(Callback) {
+        this.#Updatecallback = Callback;
+    }
+    
+
+    add(Person){
+        this.#array.push({Person})
+
+        this.#Updatecallback = this.#array
+
+    }
+
+    filterage(age){
+        const result= []
+
+        for(
+            const elem of this.#array) {
+                if(elem.eletkor === age){
+                result.push(age)
+        
+
+        }
+    }
+    this.#Updatecallback(result)
+}
+
+                
+    filtername(names){
+        const nameresult = []
+
+        for(const elem of this.#array){
+            if(elem.nev.includes(names)){
+                nameresult.push(names)
+        }
+
+    }
+    this.#Updatecallback(nameresult)
 
 }
+
+    
+}
+
 
 class Datatable{
 
